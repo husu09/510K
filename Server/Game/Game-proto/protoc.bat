8ad13add4a2ff9ca99aa0ec96ab8e03377ba9273
husu09 <husu09@qq.com>
2018-09-06 00:09:34 +0800

    n

--- Server/Game/Game-proto/protoc.bat
+++ Server/Game/Game-proto/protoc.bat
@@ -1,4 +1,5 @@
 cd /d %~dp0
 protoc --proto_path=proto --java_out=..\game-common\src\main\java\ proto\*.proto
 protoc --proto_path=proto --java_out=..\game-client\src\main\java\ proto\*.proto
+"protogen 2.3.16\net462\protogen" --proto_path=.\\proto\\ *.proto --csharp_out=csharp-proto +langver=3
 pause
