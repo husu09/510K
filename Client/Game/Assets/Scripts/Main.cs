e1123bf3035d86795029a3f778eea4d9ba57d9df
husu09 <husu09@qq.com>
2018-09-06 08:30:54 +0800

    n

---
+++ Client/Game/Assets/Scripts/Main.cs
@@ -0,0 +1,18 @@
+﻿using ProtoBuf;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Serializer.Deserialize()
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
