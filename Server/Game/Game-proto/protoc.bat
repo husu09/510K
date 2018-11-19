d8ecb989301d942bff2590deca82685779025b5a
husu09 <husu09@qq.com>
2018-11-19 21:00:29 +0800

    n

--- Server/Game/Game-proto/protoc.bat
+++ Server/Game/Game-proto/protoc.bat
@@ -1,5 +1,6 @@
 cd /d %~dp0
 protoc --proto_path=proto --java_out=..\game-common\src\main\java\ proto\*.proto
 protoc --proto_path=proto --java_out=..\game-client\src\main\java\ proto\*.proto
+::protoc --proto_path=proto --js_out=import_style=commonjs,binary:.\js-proto\ .\proto\*.proto
 "protogen 2.3.16\net462\protogen" --proto_path=.\\proto\\ *.proto --csharp_out=csharp-proto +langver=3
 pause
