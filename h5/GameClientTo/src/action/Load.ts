4adb82a77c66c1eee465a9f0e5d605da34598684
husu09 <husu09@qq.com>
2018-12-01 17:33:42 +0800

    n

--- h5/GameClientTo/src/action/Load.ts
+++
@@ -1,7 +0,0 @@
-export default class Load extends Laya.Script {
-    onEnable(): void {
-        Laya.timer.once(5000, this, () => {
-            Laya.Scene.open("Main.scene")
-        });
-    }
-}
---
+++ h5/GameClientTo/src/action/Main.ts
@@ -0,0 +1,5 @@
+export default class Main extends Laya.Script {
+    onEnable(): void {
+        
+    }
+}
